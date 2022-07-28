77cc84f77b40e9cfc5d9ac30c31e8676769d1ff2
Denis <denis.sivec1@gmai.com>
2022-07-28 23:58:07 +0200

    testdenis

---
+++ app/src/main/java/com/example/tva_projekt/Hribovje.java
@@ -0,0 +1,11 @@
+package com.example.tva_projekt;
+
+public class Hribovje {
+    String ime;
+    int stVrhov;
+
+    public Hribovje(String ime, int stVrhov) {
+        this.ime = ime;
+        this.stVrhov = stVrhov;
+    }
+}
---
+++ app/src/main/java/com/example/tva_projekt/ListAdapterHribi.java
@@ -0,0 +1,41 @@
+package com.example.tva_projekt;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class ListAdapterHribi extends ArrayAdapter<Hribovje> {
+    public ListAdapterHribi(Context context, ArrayList<Hribovje> hribovjeArrayList){
+        super(context, R.layout.list_item, hribovjeArrayList);
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+
+        Hribovje hribovje = getItem(position);
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+
+        }
+        TextView gorovje = convertView.findViewById(R.id.gorovje);
+        TextView gorovjeSteviloGor = convertView.findViewById(R.id.gorovjeSteviloGor);
+
+        gorovje.setText(hribovje.ime);
+        gorovjeSteviloGor.setText(Integer.toString(hribovje.stVrhov));
+
+
+        return convertView;
+
+
+    }
+}
