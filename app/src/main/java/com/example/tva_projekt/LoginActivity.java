d2d4b141cf0146fa0968df21abddab307265d669
Jan Kačičnik <jan.kacicnik@gmail.com>
2022-07-28 17:54:28 +0200

    test

--- app/src/main/java/com/example/tva_projekt/LoginActivity.java
+++ app/src/main/java/com/example/tva_projekt/LoginActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.BoringLayout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
+    DBHelper DB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         TextView btn=findViewById(R.id.textViewSignup);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +29,33 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
+
+        username=(EditText) findViewById(R.id.inputUsername1);
+        password=(EditText)  findViewById(R.id.inputPassword1);
+        login=(Button) findViewById(R.id.Login);
+        DB=new DBHelper(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user=username.getText().toString();
+                String pass=password.getText().toString();
+
+                if (user.equals("")||pass.equals(""))
+                    Toast.makeText(LoginActivity.this,"Please enter all fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass=DB.checkusernamepassword(user,pass);
+                    if (checkuserpass==true){
+                        Toast.makeText(LoginActivity.this,"Sign in succesfull",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(LoginActivity.this,"Invalid credentials",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
 }
