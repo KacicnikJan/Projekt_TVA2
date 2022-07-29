ded579c50ed154514c643672c1e8a7a04392556f
Jan Kačičnik <jan.kacicnik@gmail.com>
2022-07-29 19:21:45 +0200

    test

--- app/src/main/java/com/example/tva_projekt/HomeActivity.java
+++ app/src/main/java/com/example/tva_projekt/HomeActivity.java
@@ -2,13 +2,44 @@ package com.example.tva_projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button izbiriHriba;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        TextView btn = findViewById(R.id.btnLogout);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, HomeActivity.class));
+            }
+        });
+
+        username = (EditText) findViewById(R.id.inputUsername1);
+        password = (EditText) findViewById(R.id.inputPassword1);
+        izbiriHriba = (Button) findViewById(R.id.btnRazlizcnePoti);
+        DB = new DBHelper(this);
+
+        izbiriHriba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(),Hribi_Izbira.class);
+                startActivity(i);
+            }
+        });
     }
 }
